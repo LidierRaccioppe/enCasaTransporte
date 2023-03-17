889b52b706647f406a94a16187458eb9de64b91c
Lidier López <llopezraccioppe@danielcastelao.org>
2023-03-17 09:20:09 +0100

    Reconfigurada la Interface para tener javadoc

--- src/ITransporte.java
+++ src/ITransporte.java
@@ -1,11 +1,16 @@
 /**
- * Fabrica que devuelve el transporte
- * segun el tipo de embalaje
+ * Posible empaquetados de los envios
  */
 public interface ITransporte {
 
         public static final int PALET = 0;
         public static final int CAJACARTON = 1;
         public static final int CAJAMADERA = 2;
+
+        /**
+         * Abstracto que obliga a ingresar un Codigo Postal, y delimitara el coste
+         * @param CodigoPostal codigo de ubicacion de una casa
+         * @return el precio final por envio
+         */
     public Float costeTotal ( Integer CodigoPostal);
 }
