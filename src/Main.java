6f468b6a6dcc6f401cb94872dbe2fea1567f9276
Lidier López <llopezraccioppe@danielcastelao.org>
2023-03-17 09:20:28 +0100

    Reconfigurada la Main para tener javadoc y Usar Barco

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,39 @@
 import javax.swing.*;
 
+/**
+ * La Main o App
+ */
 public class Main {
-
+    /**
+     * Objeto General para usar los metodos
+     */
     static ITransporte it; // es un default
     public static void main(String[ ] args){
+        /**
+         * almacena el tipo de trasporte que puede ser
+         */
         String tipo = null;//objetos puedne ser nulos
+        /**
+         * almacena el precio final por el trasporte
+         */
         Float costeTotal = null; //objetos pueden ser nulos
+        /**
+         * Afectara segun que empaqueta se usa (o embalaje)
+         */
         Integer numeroDelTipo = null;//Integer objeto de int a groso modo  --- para embalaje
+        /**
+         * Agarre para elegir la clase de envio
+         */
 
-        int palanca = Integer.parseInt(JOptionPane.showInputDialog("Ingrese 1 o 2    para  CAMION  o  BICICLETA dependiendo de con cual quiere que se lo enviemos, respectivamente"));
+        int palanca = Integer.parseInt(JOptionPane.showInputDialog("Ingrese 1 o 2 o 3   para  CAMION  o  BICICLETA   o  CAMION dependiendo de con cual quiere que se lo enviemos, respectivamente"));
 
         switch(palanca){
 
             case 1: it = FactoryTransport.getProduct(FactoryTransport.CAMION);
                 break;
             case 2: it = FactoryTransport.getProduct(FactoryTransport.BICICLETA);
+                break;
+            case 3: it = FactoryTransport.getProduct(FactoryTransport.BARCO);
                 break;
             default: break;
         }
