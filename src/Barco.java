0215d7b373e0e814d4fd7c8ca8de035a937ea38b
Lidier López <llopezraccioppe@danielcastelao.org>
2023-03-17 09:18:49 +0100

    Terminada la Clase Barco

--- src/Barco.java
+++ src/Barco.java
@@ -1,9 +1,43 @@
-public class Barco {
+/**
+ * Clase que especifica el metodo de transporte, siendo este un barco
+ * segun el FactoryTransport
+ */
+public class Barco implements ITransporte{
+        /**
+         * El codigo de la recidencia de una edificio
+         */
+        private Integer codigoPostal;
+        /**
+         * Linea de Tierra, eje horizontal, largo, otro nombres.
+         */
+        private Float ejeX;
+        /**
+         * Linea de aire, eje vertical, altura, otros nombre.
+         */
+        private Float ejeY;
+        /**
+         * Linea de Fondo, eje de profundidad, profuntidad.
+         */
+        private Float ejeZ;
+        /**
+         * El peso cambia el coste, por el desgasto de mantener tal peso.
+         */
+        private Float peso;
+        public Barco(Integer codigoPostal, Float ejeX, Float ejeY, Float ejeZ, Float peso) {
+                this.codigoPostal = codigoPostal;
+                this.ejeX = ejeX;
+                this.ejeY = ejeY;
+                this.ejeZ = ejeZ;
+                this.peso = peso;
+        }
 
-
-
-
-
-
-
+        /**
+         *
+         * @param CodigoPostall
+         * @return precio  Parte de lo que tendra que pagar por el envio, sin tener en cuenta el impuesto de transporte o de aduana
+         */
+        public Float costeTotal(Integer CodigoPostall){
+                Float precio = codigoPostal.floatValue()*0+10;
+                return precio;
+        }
 }
