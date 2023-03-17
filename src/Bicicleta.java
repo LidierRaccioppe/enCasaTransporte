524a02cb0307c107d8fb0e1345bb2e248b486ee4
Lidier López <llopezraccioppe@danielcastelao.org>
2023-03-17 09:25:56 +0100

    Reconfigurada las clases, Bicicleta y Camion para tener JavaDoc

--- src/Bicicleta.java
+++ src/Bicicleta.java
@@ -1,9 +1,27 @@
+/**
+ * Clase que especifica el metodo de transporte, siendo este una bicicleta
+ * segun el FactoryTransport
+ */
 public class Bicicleta implements ITransporte{
-
+    /**
+     * El codigo de la recidencia de una edificio
+     */
     private Integer codigoPostal;
+    /**
+     * Linea de Tierra, eje horizontal, largo, otro nombres.
+     */
     private Float ejeX;
+    /**
+     * Linea de aire, eje vertical, altura, otros nombre.
+     */
     private Float ejeY;
+    /**
+     * Linea de Fondo, eje de profundidad, profuntidad.
+     */
     private Float ejeZ;
+    /**
+     * El peso cambia el coste, por el desgasto de mantener tal peso.
+     */
     private Float peso;
 
     public Bicicleta(Integer codigoPostal, Float ejeX, Float ejeY, Float ejeZ, Float peso) {
@@ -14,6 +32,11 @@ public class Bicicleta implements ITransporte{
         this.peso = peso;
     }
 
+    /**
+     *
+     * @param CodigoPostall
+     * @return precio  Parte de lo que tendra que pagar por el envio, sin tener en cuenta el impuesto de transporte o de aduana
+     */
     public Float costeTotal(Integer CodigoPostall){
         Float precio = codigoPostal.floatValue()*0+5;
         return precio;
--- src/Camion.java
+++ src/Camion.java
@@ -1,8 +1,27 @@
+/**
+ * Clase que especifica el metodo de transporte, siendo este un camion
+ * segun el FactoryTransport
+ */
 public class Camion implements ITransporte{
+        /**
+         * El codigo de la recidencia de una edificio
+         */
         private Integer codigoPostal;
+        /**
+         * Linea de Tierra, eje horizontal, largo, otro nombres.
+         */
         private Float ejeX;
+        /**
+         * Linea de aire, eje vertical, altura, otros nombre.
+         */
         private Float ejeY;
+        /**
+         * Linea de Fondo, eje de profundidad, profuntidad.
+         */
         private Float ejeZ;
+        /**
+         * El peso cambia el coste, por el desgasto de mantener tal peso.
+         */
         private Float peso;
 
         public Camion(Integer codigoPostal, Float ejeX, Float ejeY, Float ejeZ, Float peso) {
@@ -13,6 +32,11 @@ public class Camion implements ITransporte{
                 this.peso = peso;
         }
 
+        /**
+         *
+         * @param CodigoPostall
+         * @return precio  Parte de lo que tendra que pagar por el envio, sin tener en cuenta el impuesto de transporte o de aduana
+         */
         public Float costeTotal(Integer CodigoPostall){
                 Float precio = codigoPostal.floatValue()*0+2;
                 return precio;
