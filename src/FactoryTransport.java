a3aa4f99200f582c98d8b8be0bb125e36dbed897
Lidier López <llopezraccioppe@danielcastelao.org>
2023-03-17 09:19:31 +0100

    Reconfigurada la Clase FactoryTransport para usar Barco

--- src/FactoryTransport.java
+++ src/FactoryTransport.java
@@ -1,17 +1,38 @@
 import javax.swing.*;
 
 /**
- * Fabrica que devuelve el transporte
- * segun el tipo de embalaje
+ * Zona de construccion
  */
 public class FactoryTransport {
         public static final int CAMION = 1;
         public static final int BICICLETA = 2;
+        public static final int BARCO = 3;
+        /**
+         * El codigo de la recidencia de una edificio
+         */
         public static  Integer codigoDePostal = null;
+        /**
+         * Linea de Tierra, eje horizontal, largo, otro nombres.
+         */
         public static  Float lineaX = null;
+        /**
+         * Linea de aire, eje vertical, altura, otros nombre.
+         */
         public static  Float lineaY = null;
+        /**
+         * Linea de Fondo, eje de profundidad, profuntidad.
+         */
         public static  Float lineaZ = null;
+        /**
+         * El peso cambia el coste, por el desgasto de mantener tal peso.
+         */
         public static  Float peso = null;
+
+        /**
+         * Obtener el producto, por el medio de la FactoryTransport
+         * @param tipo sera la palanca que permitira cambiar el switch
+         * @return
+         */
         public static ITransporte getProduct(int tipo){
                 codigoDePostal = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su codigo postal"));
                 lineaX = Float.valueOf(Integer.parseInt(JOptionPane.showInputDialog("Ingrese la longitud de la linea de tierra")));
@@ -21,6 +42,7 @@ public class FactoryTransport {
                 switch (tipo){
                         case CAMION: return new Camion(codigoDePostal,lineaX,lineaY,lineaZ,peso);
                         case BICICLETA: return new Bicicleta(codigoDePostal,lineaX,lineaY,lineaZ,peso);
+                        case BARCO: return new Barco(codigoDePostal,lineaX,lineaY,lineaZ,peso);
                         default: return null;
                 }
         }
