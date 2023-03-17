61eadcd5636f18815516251e558e3187025d2bd2
Lidier Raccioppe <llopezraccioppe@danielcastelao.org>
2023-03-17 06:54:10 +0100

    Facotorizado

--- src/FactoryTransport.java
+++ src/FactoryTransport.java
@@ -1,21 +1,28 @@
+import javax.swing.*;
+
 /**
  * Fabrica que devuelve el transporte
  * segun el tipo de embalaje
  */
 public class FactoryTransport {
-        public static final int PALET = 0;
-        public static final int CAJACARTON = 1;
-        public static final int CAJAMADERA = 2;
-
-
-
-
-
-
-
-
-
-
-
-
+        public static final int CAMION = 1;
+        public static final int BICICLETA = 2;
+        public static  Integer codigoDePostal = null;
+        public static  Float lineaX = null;
+        public static  Float lineaY = null;
+        public static  Float lineaZ = null;
+        public static  Float peso = null;
+        public static ITransporte getProduct(int tipo){
+                codigoDePostal = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su codigo postal"));
+                lineaX = Float.valueOf(Integer.parseInt(JOptionPane.showInputDialog("Ingrese la longitud de la linea de tierra")));
+                lineaY = Float.valueOf(Integer.parseInt(JOptionPane.showInputDialog("Ingrese la longitud de la linea de aire")));
+                lineaZ = Float.valueOf(Integer.parseInt(JOptionPane.showInputDialog("Ingrese la profundidad")));
+                peso = Float.valueOf(Integer.parseInt(JOptionPane.showInputDialog("Ingrese el peso")));
+                switch (tipo){
+                        case CAMION: return new Camion(codigoDePostal,lineaX,lineaY,lineaZ,peso);
+                        case BICICLETA: return new Bicicleta(codigoDePostal,lineaX,lineaY,lineaZ,peso);
+                        default: return null;
+                }
+        }
+        {} //en casa anda mal
 }
---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class Main {
+
+    static ITransporte it; // es un default
+    public static void main(String[ ] args){
+        String tipo = null;//objetos puedne ser nulos
+        Float costeTotal = null; //objetos pueden ser nulos
+        Integer numeroDelTipo = null;//Integer objeto de int a groso modo  --- para embalaje
+
+
+        int palanca = Integer.parseInt(JOptionPane.showInputDialog("Ingrese 1 o 2    para  CAMION  o  BICICLETA dependiendo de con cual quiere que se lo enviemos, respectivamente"));
+
+
+        switch(palanca){
+
+            case 1: it = FactoryTransport.getProduct(FactoryTransport.CAMION);
+                break;
+            case 2: it = FactoryTransport.getProduct(FactoryTransport.BICICLETA);
+                break;
+            default: break;
+        }
+        costeTotal = it.costeTotal(FactoryTransport.codigoDePostal);
+
+        System.out.println("Coste Total =" + costeTotal);
+    }
+}
